2811b17c24157ae18dec63267a14827bb19f0bac
kenny_robert <skenrobert@gmail.com>
2023-04-29 19:21:58 -0500

    update test koutlin

---
+++ app/src/test/java/com/curso/mvvm/ui/viewmodel/QuoteViewModelTest.kt
@@ -0,0 +1,82 @@
+package com.curso.mvvm.ui.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.curso.mvvm.domain.GetQuotesUseCase
+import com.curso.mvvm.domain.GetRandomQuoteUseCase
+import com.curso.mvvm.domain.model.Quote
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+internal class QuoteViewModelTest{
+    @RelaxedMockK
+    private lateinit var getQuotesUseCase: GetQuotesUseCase
+
+    @RelaxedMockK
+    private lateinit var getRandomQuoteUseCase: GetRandomQuoteUseCase
+
+    private lateinit var quoteViewModel: QuoteViewModel
+
+    @get:Rule
+    var rule: InstantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun onBefore() {
+        MockKAnnotations.init(this)
+        quoteViewModel = QuoteViewModel(getQuotesUseCase, getRandomQuoteUseCase)
+        Dispatchers.setMain(Dispatchers.Unconfined)
+    }
+
+    @After
+    fun onAfter() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `when randomQuoteUseCase return a quote set on the livedata`() = runTest {
+        //Given
+        val quote = Quote("Holi", "kenny")
+        coEvery { getRandomQuoteUseCase() } returns quote
+
+        //When
+        quoteViewModel.randomQuote()
+
+        //Then
+        assert(quoteViewModel.quoteModel.value == quote)
+    }
+
+    @Test
+    fun `if randomQuoteUseCase return null keep the last value`() = runTest{
+        //Given
+        val quote = Quote("test", "test")
+        quoteViewModel.quoteModel.value = quote
+        coEvery { getRandomQuoteUseCase() } returns null
+
+        //When
+        quoteViewModel.randomQuote()
+
+        //Then
+        assert(quoteViewModel.quoteModel.value == quote)
+    }
+
+    @Test
+    fun `when viewmodel is created at the first time, get all quotes and set the first value`() = runTest{
+        //Given
+        val quote = listOf(Quote("Holi", "test"), Quote("Dame un like", "Otro kenny "))
+        coEvery { getQuotesUseCase() } returns quote
+
+        //When
+        quoteViewModel.onCreate()
+
+        //Then
+        assert(quoteViewModel.quoteModel.value == quote.first())
+    }
+}
